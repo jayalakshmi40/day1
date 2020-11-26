a63de8638f6bd8dd063f34c9a780f74276c245f9
jayalakshmi40 <74904536+jayalakshmi40@users.noreply.github.com>
2020-11-26 12:54:24 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,16 @@
+a=[1,2,3,4]
+a.append(5)
+print(a)
+a.remove(5)
+print(a)
+print("largest element is:",max(a))
+print("smallest element is:",min(a))
+
+a=("Internship")
+b=reversed(a)
+print(tuple(b))
+
+tuple=(1,2,3,4,5)
+list=list(tuple)
+print(type(list))
+print(list)
