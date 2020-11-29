2afc5bdd3a5b5b52d12ca45e0d32e18a6731fd31
jayalakshmi40 <74904536+jayalakshmi40@users.noreply.github.com>
2020-11-29 19:41:26 +0530

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,17 @@
+#1
+import samplemodule
+samplemodule.my_list.insert(1,8)
+samplemodule.my_list.append(6)
+print(samplemodule.my_list)
+#2
+#pandas installed successfully
+#3
+import random
+print("random integer is",random.randint(1,100))
+#4
+import sys
+print(sys.path)
+#5
+#pip install wheel #to install wheel
+#pip uninstall wheel #to uninstall wheel
+
